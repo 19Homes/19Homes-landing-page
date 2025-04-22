dfb0b926a755f12ab0ff392997c0d160f0b0fd20
Tom Okafor <tmeka008@gmail.com>
2025-04-22 21:04:05 +0100

    feat: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main className="w-full min-h-dvh bg-black-75 grid place-content-center">
+      <h1 className="font-montserrat font-bold text-[64px] text-white">
+        Sorry, You seem to have lost your way.
+      </h1>
+    </main>
+  );
+}
