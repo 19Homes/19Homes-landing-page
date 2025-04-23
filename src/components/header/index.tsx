7c3c0b8d096cfc64a5d747b21f718916983a95bc
Tom Okafor <tmeka008@gmail.com>
2025-04-23 18:09:56 +0100

    feat: add navigation component to header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,11 @@
 import Logo from "./components/logo";
+import Navigation from "./components/nav";
 
-
-export default function Header(){
-    return <header className="flex">
-        <Logo />
+export default function Header() {
+  return (
+    <header className="flex justify-between">
+      <Logo />
+      <Navigation />
     </header>
+  );
 }
