1c8ebbae6700afacfb3b4432d711425d1a74d039
Tom Okafor <tmeka008@gmail.com>
2025-04-23 19:26:54 +0100

    feat(header): add block of buttons to header, style(header): implement final styling for the header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,16 @@
 import Logo from "./components/logo";
 import Navigation from "./components/nav";
+import HeaderButton from "./components/button";
 
 export default function Header() {
   return (
-    <header className="flex justify-between">
+    <header className="flex justify-between mt-10 px-14 items-center">
       <Logo />
       <Navigation />
+      <div className="flex gap-4">
+        <HeaderButton classnames="border border-gold-100 text-gold-100 bg-white" text="login" />
+        <HeaderButton classnames="bg-gold-100 text-white" text="sign up" />
+      </div>
     </header>
   );
 }
