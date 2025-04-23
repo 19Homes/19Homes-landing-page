5220d02cabc4507823a84067f990954c7a42ae6e
Tom Okafor <tmeka008@gmail.com>
2025-04-23 18:28:31 +0100

    feat: create active state for the navlinks and add a different text color to the currently active state, style: add hover interactivity to navlinks

--- src/components/header/components/nav/index.tsx
+++ src/components/header/components/nav/index.tsx
@@ -1,14 +1,28 @@
+"use client";
+
 import Link from "next/link";
 import { navigationLinks } from "../../constants";
+import { useState } from "react";
+import { cn } from "@/utils/classnames";
 
 export default function Navigation() {
+  const [isActive, setActiveState] = useState<number>(0);
+  const handleLinkClick = (index: number) => {
+    setActiveState(index);
+  };
   return (
     <nav className="flex gap-14">
       {navigationLinks.map((link, index) => (
         <Link
           key={index}
           href={link.path}
-          className="text-black-100 font-montserrat font-medium text-sm"
+          className={cn(
+            "font-montserrat font-medium text-sm capitalize hover:text-gold-75 duration-300",
+            index === isActive ? "text-gold-100" : "text-black-100"
+          )}
+          onClick={() => {
+            handleLinkClick(index);
+          }}
         >
           {link.text}
         </Link>
