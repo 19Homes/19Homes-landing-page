82d7bffe62d76d399eefd27780a23f7353bd1ebc
Tom Okafor <tmeka008@gmail.com>
2025-04-23 19:00:06 +0100

    feat: add chevron icon to nav link with child component

--- src/components/header/components/nav/index.tsx
+++ src/components/header/components/nav/index.tsx
@@ -5,7 +5,7 @@ import { navigationLinks } from "../../constants";
 import { useState } from "react";
 import { cn } from "@/utils/classnames";
 import UseActiveLinkIndex from "../../hooks/useActiveLink";
-
+import { ChevronDown } from "lucide-react";
 export default function Navigation() {
   const [isActive, setActiveState] = useState<number>(UseActiveLinkIndex());
   const handleLinkClick = (index: number) => {
@@ -18,14 +18,27 @@ export default function Navigation() {
           key={index}
           href={link.path}
           className={cn(
-            "font-montserrat font-medium text-sm capitalize hover:text-gold-75 duration-300",
+            "font-montserrat font-medium text-sm capitalize hover:text-gold-75 duration-300 group",
             index === isActive ? "text-gold-100" : "text-black-100"
           )}
           onClick={() => {
             handleLinkClick(index);
           }}
         >
-          {link.text}
+          {link.hasChild ? (
+            <span className="flex gap-2 items-center">
+              <span>{link.text}</span>
+              <ChevronDown
+                size={16}
+                className={cn(
+                  "group-hover:rotate-180 duration-300 group-hover:text-gold-75",
+                  index === isActive ? "text-gold-100" : "text-[#1C1B1F]"
+                )}
+              />
+            </span>
+          ) : (
+            link.text
+          )}
         </Link>
       ))}
     </nav>
