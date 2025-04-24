00848cc7519d2f1bfa3be03e3f6d63a38c0530eb
Tom Okafor <tmeka008@gmail.com>
2025-04-24 05:13:38 +0100

    style(footer): capitalize footer categories texts and add hover interactivity to links

--- src/components/footer/components/footer-categories-block/index.tsx
+++ src/components/footer/components/footer-categories-block/index.tsx
@@ -3,7 +3,7 @@ import { footerCategories } from "../../constants";
 
 export default function FooterCategoriesBlock() {
   return (
-    <section className="space-y-8">
+    <section className="space-y-8 capitalize">
       <h3 className="text-2xl text-white font-poppins font-bold text-center">
         {footerCategories.blockTitle}
       </h3>
@@ -11,7 +11,7 @@ export default function FooterCategoriesBlock() {
         {footerCategories.items.map((item, index) => (
           <li
             key={index}
-            className="font-montserrat font-medium text-white text-sm text-center"
+            className="font-montserrat font-medium text-white text-sm text-center hover:text-gold-100 duration-200"
           >
             <Link href={item.path}>{item.name}</Link>
           </li>
