27483234cd58aa480949535983da77de05f6a7f4
Tom Okafor <tmeka008@gmail.com>
2025-04-24 04:02:33 +0100

    feat: set up static data for footer section

---
+++ src/components/footer/constants/index.ts
@@ -0,0 +1,37 @@
+import { FooterCategoriesTypes } from "../types";
+import { FooterServicesTypes } from "../types";
+import { FooterContactTypes } from "../types";
+
+export const footerCategories: FooterCategoriesTypes = {
+  blockTitle: "Categories",
+  items: [
+    { name: "home", path: "/" },
+    { name: "about us", path: "/about" },
+    { name: "career", path: "/career" },
+    { name: "properties", path: "/properties" },
+    { name: "contact", path: "/contact" },
+  ],
+};
+
+export const footerServices: FooterServicesTypes = {
+  blockTitle: "services",
+  items: ["house sales", "land sales", "buildings", "architectural designs"],
+};
+
+export const footerContact: FooterContactTypes = {
+  blockTitle: "Contact",
+  items: [
+    {
+      detail: "3, idumota street, Lagos Island Nigeria",
+      icon: "/footer/distance.svg",
+    },
+    {
+      detail: "+2349035064848",
+      icon: "/footer/call.svg",
+    },
+    {
+      detail: "19homesempire@gmail.com",
+      icon: "/footer/mail.svg",
+    },
+  ],
+};
