16aca365ffaf385d7d8457391933c0afa861edae
Tom Okafor <tmeka008@gmail.com>
2025-04-24 09:24:10 +0100

    feat: populate footer main block with all sub components

--- src/components/footer/components/footer-main-block/index.tsx
+++ src/components/footer/components/footer-main-block/index.tsx
@@ -1,9 +1,17 @@
 import FooterTextAndLogoBlock from "../footer-text-and-logo-block";
+import FooterCategoriesBlock from "../footer-categories-block";
+import FooterServicesBlock from "../footer-services-block";
+import FooterContactBlock from "../footer-contact-block";
 
 export default function FooterMainBlock() {
   return (
     <section className="flex gap-[52px]">
       <FooterTextAndLogoBlock />
+      <section className="w-full flex justify-between">
+        <FooterCategoriesBlock />
+        <FooterServicesBlock />
+        <FooterContactBlock />
+      </section>
     </section>
   );
 }
