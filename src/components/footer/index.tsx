b0ea2736bd8c00ece30a215822c1637133f55389
Tom Okafor <tmeka008@gmail.com>
2025-04-24 10:50:46 +0100

    style(footer):create css module for footer section and add scale animation to the background image

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,11 +1,14 @@
 import { useCallback } from "react";
 import FooterMainBlock from "./components/footer-main-block";
+import styles from "./styles/styles.module.css";
 export default function Footer() {
   const currentYear = useCallback(() => {
     return new Date().getFullYear();
   }, []);
   return (
-    <footer className="w-full h-[502px] bg-[linear-gradient(rgba(20,20,20,0.92),rgba(20,20,20,0.75)),url('/footer/footer-image.png')] bg-cover py-[49px] px-14 flex flex-col gap-20">
+    <footer
+      className={`w-full h-[502px] bg-[linear-gradient(rgba(20,20,20,0.92),rgba(20,20,20,0.75)),url('/footer/footer-image.png')] bg-cover py-[49px] px-14 flex flex-col gap-20 ${styles.footer}`}
+    >
       <FooterMainBlock />
       <section className="flex flex-col w-full gap-2.5 items-center">
         <div className="w-full h-[5px] bg-white rounded-full"></div>
