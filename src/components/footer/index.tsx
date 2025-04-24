2c9b93ae71f09c060ab97cdcfda92c087a92d295
Tom Okafor <tmeka008@gmail.com>
2025-04-24 04:32:54 +0100

    feat(footer): create basic structure and styling for footer section

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+  return (
+    <footer className="w-full h-[502px] bg-[linear-gradient(rgba(20,20,20,0.92),rgba(20,20,20,0.75)),url('/footer/footer-image.png')] bg-cover py-[49px] px-14]"></footer>
+  );
+}
