ba01f6ce238c2f3cc123d735a622e3f0f27a533e
Tom Okafor <tmeka008@gmail.com>
2025-04-25 09:17:03 +0100

    feat(blog section): create blog posts dummy data

---
+++ src/components/blog/constants/index.ts
@@ -0,0 +1,22 @@
+import { BlogPost } from "../types";
+
+export const blogPosts: BlogPost[] = [
+  {
+    title: "real estate investment benefits",
+    image: "/blog-section/blog-post.png",
+    descriptipn:
+      "Lorem ipsum dolor sit amet consectetur. Et aliquam proin pulvinar enim senectus vitae quis aliquam. Pharetra nunc eget viverra magna blandit. Eget dictum sit netus at.",
+  },
+  {
+    title: "the future of real estate",
+    image: "/blog-section/blog-post2.png",
+    descriptipn:
+      "Lorem ipsum dolor sit amet consectetur. Et aliquam proin pulvinar enim senectus vitae quis aliquam. Pharetra nunc eget viverra magna blandit. Eget dictum sit netus at.",
+  },
+  {
+    title: "why you should invest in real estate",
+    image: "/blog-section/blog-post3.png",
+    descriptipn:
+      "Lorem ipsum dolor sit amet consectetur. Et aliquam proin pulvinar enim senectus vitae quis aliquam. Pharetra nunc eget viverra magna blandit. Eget dictum sit netus at.",
+  },
+];
