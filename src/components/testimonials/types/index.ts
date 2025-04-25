688c5a4bee96935440e98cc1bd3150e002c43472
Tom Okafor <tmeka008@gmail.com>
2025-04-25 14:26:25 +0100

    refactor(testimonial types): add type for tesimonial card props and change testimonial card type to inherit properties from testimonialcardprops adding the stars property to itself

--- src/components/testimonials/types/index.ts
+++ src/components/testimonials/types/index.ts
@@ -1,9 +1,13 @@
 import { StaticImageData } from "next/image";
 
-export type TestimonialCard = {
+
+export type TestimonialCardProps = {
   name: string;
   role: string;
   text: string;
   image: StaticImageData;
+};
+
+export type TestimonialCard = TestimonialCardProps & {
   stars: number;
 };
