079bf32af46d814d9554dc4103a47d9508478748
Tom Okafor <tmeka008@gmail.com>
2025-04-26 07:43:13 +0100

    feat(services): create data for sevices cards

---
+++ src/components/services/constants/index.ts
@@ -0,0 +1,32 @@
+import { CardBase } from "@/types";
+import sales from "../../../../public/services/house-sales.png";
+import building from "../../../../public/services/building.png";
+import architecturalDesign from "../../../../public/services/architectural-design.png";
+import interiorDesign from "../../../../public/services/interior.jpg";
+
+export const ServicesCardsData: CardBase[] = [
+  {
+    title: "house and land sales",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Nisl enim tincidunt iaculis risus porttitor metus purus. Scelerisque at faucibus diam faucibus ultrices dignissim natoque mattis mattis. ",
+    image: sales,
+  },
+  {
+    title: "building",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Nisl enim tincidunt iaculis risus porttitor metus purus. Scelerisque at faucibus diam faucibus ultrices dignissim natoque mattis mattis. ",
+    image: building,
+  },
+  {
+    title: "architectural design",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Nisl enim tincidunt iaculis risus porttitor metus purus. Scelerisque at faucibus diam faucibus ultrices dignissim natoque mattis mattis. ",
+    image: architecturalDesign,
+  },
+  {
+    title: "interior design",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Nisl enim tincidunt iaculis risus porttitor metus purus. Scelerisque at faucibus diam faucibus ultrices dignissim natoque mattis mattis. ",
+    image: interiorDesign,
+  },
+];
