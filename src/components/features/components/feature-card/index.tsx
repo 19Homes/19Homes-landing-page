0c6768a945cd212545cf7093dc00a57a7049a7c3
Tom Okafor <tmeka008@gmail.com>
2025-04-26 10:42:08 +0100

    feat(features): create feature card component.
    This component represents a single feature card that could be used to showcase the various features of the organization

---
+++ src/components/features/components/feature-card/index.tsx
@@ -0,0 +1,24 @@
+import { CardBase } from "@/types";
+import Image from "next/image";
+
+export default function FeatureCard({ title, description, image }: CardBase) {
+  return (
+    <article className="shrink-0 flex flex-col items-center justify-center gap-8 rounded-4xl p-6 w-[450px] shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
+      <div className="w-full flex flex-col gap-6 items-center">
+        <Image
+          src={image}
+          alt={`${title} icon`}
+          aria-hidden="true"
+          width={50}
+          height={50}
+        />
+        <h2 className="capitalize text-center text-3xl text-black-100 font-poppins font-semibold">
+          {title}
+        </h2>
+      </div>
+      <p className="text-2xl text-black-100 font-poppins font-normal">
+        {description}
+      </p>
+    </article>
+  );
+}
