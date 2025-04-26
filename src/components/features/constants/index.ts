7f61d1ed979d739d5aa9b43af377ecd38134829c
Tom Okafor <tmeka008@gmail.com>
2025-04-26 10:25:01 +0100

    feat(features): create data for features section cards

---
+++ src/components/features/constants/index.ts
@@ -0,0 +1,46 @@
+import { CardBase } from "@/types";
+import commitment from "../../../../public/features/commitment.svg";
+import expertise from "../../../../public/features/expertise.svg";
+import satisfaction from "../../../../public/features/satisfaction.svg";
+import service from "../../../../public/features/service.svg";
+import advice from "../../../../public/features/advise.svg";
+import opportunities from "../../../../public/features/opportunities.svg";
+
+export const featuresData: CardBase[] = [
+  {
+    title: "our commitment",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Nisl enim tincidunt iaculis risus porttitor metus purus. Scelerisque at faucibus diam faucibus ultrices dignissim natoque mattis mattis. ",
+    image: commitment,
+  },
+  {
+    title: "our expertise",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Nisl enim tincidunt iaculis risus porttitor metus purus. Scelerisque at faucibus diam faucibus ultrices dignissim natoque mattis mattis. ",
+    image: expertise,
+  },
+  {
+    title: "customer satisfaction",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Nisl enim tincidunt iaculis risus porttitor metus purus. Scelerisque at faucibus diam faucibus ultrices dignissim natoque mattis mattis. ",
+    image: satisfaction,
+  },
+  {
+    title: "exceptional service",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Nisl enim tincidunt iaculis risus porttitor metus purus. Scelerisque at faucibus diam faucibus ultrices dignissim natoque mattis mattis. ",
+    image: service,
+  },
+  {
+    title: "personalized advice",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Nisl enim tincidunt iaculis risus porttitor metus purus. Scelerisque at faucibus diam faucibus ultrices dignissim natoque mattis mattis. ",
+    image: advice,
+  },
+  {
+    title: "investment opportunites",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Nisl enim tincidunt iaculis risus porttitor metus purus. Scelerisque at faucibus diam faucibus ultrices dignissim natoque mattis mattis. ",
+    image: opportunities,
+  },
+];
