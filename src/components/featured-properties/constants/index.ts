58e10a534313f6c5e7072a69da34567479cb3752
Tom Okafor <tmeka008@gmail.com>
2025-05-01 11:13:36 +0100

    feat(featured properties): create data for the featured properites cards

---
+++ src/components/featured-properties/constants/index.ts
@@ -0,0 +1,32 @@
+import { FeaturedPropertyCardType } from "../types";
+import cornerstoneDuplex from "../../../../public/featured-properties/cornerstone duplex.png";
+import oneStoryBuilding from "../../../../public/featured-properties/one story building.png";
+import whitedge from "../../../../public/featured-properties/whiteedge duplex.png";
+import bathrooms from "../../../../public/featured-properties/bathtub.svg";
+import livingrooms from "../../../../public/featured-properties/chair.svg";
+import area from "../../../../public/featured-properties/responsive_layout.svg";
+import { StaticImageData } from "next/image";
+
+export const featuredPropertyCardDetails: FeaturedPropertyCardType[] = [
+  {
+    name: "cornerstone duplex",
+    price: "&#8358;25,000,000.00",
+    image: cornerstoneDuplex,
+  },
+  {
+    name: "one story building",
+    price: "&#8358;75,000,000.00",
+    image: oneStoryBuilding,
+  },
+  { name: "whitedge duplex", price: "&#8358;75,000,000.00", image: whitedge },
+];
+
+export const cardInfoIconDetails: {
+  icon: StaticImageData;
+  value: string;
+  alt: string;
+}[] = [
+  { icon: bathrooms, value: "2", alt: "bathrooms" },
+  { icon: livingrooms, value: "3", alt: "living rooms" },
+  { icon: area, value: "3000 Sqft", alt: "Area" },
+];
