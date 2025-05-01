f5f17cc9da8f93847c223fe07c2b3ac5b68434e6
Tom Okafor <tmeka008@gmail.com>
2025-05-01 23:52:51 +0100

    style(featured properties): add decorative box around the featured properties card

--- src/components/featured-properties/components/section-card/index.tsx
+++ src/components/featured-properties/components/section-card/index.tsx
@@ -1,6 +1,7 @@
 import SectionCardInfo from "../section-card-info";
 import Image from "next/image";
 import { FeaturedPropertyCardType } from "../../types";
+import CardButton from "../card-button";
 
 export default function SectionCard({
   image,
@@ -8,7 +9,7 @@ export default function SectionCard({
   price,
 }: FeaturedPropertyCardType) {
   return (
-    <article className="flex flex-col rounded-4xl shadow-card w-[350px] h-[591px]">
+    <article className="flex flex-col items-center relative rounded-4xl shadow-card w-[350px] h-[591px] bg-white">
       <div className="relative w-full h-[350px]">
         <Image
           src={image}
@@ -17,9 +18,11 @@ export default function SectionCard({
           className="object-cover rounded-t-4xl"
         />
       </div>
-      <div className="w-full h-[241px] p-5">
+      <div className="w-full h-[241px] p-5 flex flex-col items-center">
         <SectionCardInfo name={name} price={price} />
+        <CardButton />
       </div>
+      <div className="absolute w-[378px] h-[595px] rounded-4xl -z-10 border-2 border-gold-25 top-[-14px] left-[-14px]"></div>
     </article>
   );
 }
