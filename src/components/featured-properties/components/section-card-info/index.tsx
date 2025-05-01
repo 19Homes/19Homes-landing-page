4d413486ad5990bbc1008ffe092e91869dfa0701
Tom Okafor <tmeka008@gmail.com>
2025-05-01 11:19:43 +0100

    feat(featured properties): create section card info component for the featured properties section
    This component will hold the text details for each featured property. It will be attached to the section card component along with an image to make up the card

---
+++ src/components/featured-properties/components/section-card-info/index.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import { cardInfoIconDetails } from "../../constants";
+export default function SectionCardInfo({
+  name,
+  price,
+}: {
+  name: string;
+  price: string;
+}) {
+  return (
+    <div className="w-full flex flex-col gap-6">
+      <div className="flex justify-baseline w-full">
+        <h4 className="text-xl font-semibold font-poppins text-black-100">
+          {name}
+        </h4>
+        <h6 className="text-sm text-gold-100 font-poppins font-semibold">
+          {price}
+        </h6>
+      </div>
+      <div className="flex gap-[6px] items-center">
+        <Image
+          src="/featured-properties/distance.svg"
+          alt="location"
+          height={20}
+          width={20}
+        />
+        <p className="font-montserrat font-medium text-black-50 text-sm">
+          34, Banana island, Lagos, Nigeria
+        </p>
+      </div>
+      <ul className="flex gap-11 items-center">
+        {cardInfoIconDetails.map(({ icon, alt, value }, index) => (
+          <li key={index} className="flex items-center gap-[5px]">
+            <Image src={icon} alt={alt} width={15} height={15} />
+            <p className="text-xs text-black-100 font-montserrat font-normal">
+              {value}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
