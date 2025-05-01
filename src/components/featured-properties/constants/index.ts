65a23c23f148ebf3d2d8c5f2ff4099e2043df64e
Tom Okafor <tmeka008@gmail.com>
2025-05-01 23:12:38 +0100

    fix(featured properties): fix typo in the prices and change import for cornerstone duplex image

--- src/components/featured-properties/constants/index.ts
+++ src/components/featured-properties/constants/index.ts
@@ -1,5 +1,5 @@
 import { FeaturedPropertyCardType } from "../types";
-import cornerstoneDuplex from "../../../../public/featured-properties/cornerstone duplex.png";
+import cornerstoneDuplex from "../../../../public/newsletter/newsletter.jpg";
 import oneStoryBuilding from "../../../../public/featured-properties/one story building.png";
 import whitedge from "../../../../public/featured-properties/whiteedge duplex.png";
 import bathrooms from "../../../../public/featured-properties/bathtub.svg";
@@ -10,15 +10,15 @@ import { StaticImageData } from "next/image";
 export const featuredPropertyCardDetails: FeaturedPropertyCardType[] = [
   {
     name: "cornerstone duplex",
-    price: "&#8358;25,000,000.00",
+    price: "₦25,000,000.00",
     image: cornerstoneDuplex,
   },
   {
     name: "one story building",
-    price: "&#8358;75,000,000.00",
+    price: "₦75,000,000.00",
     image: oneStoryBuilding,
   },
-  { name: "whitedge duplex", price: "&#8358;75,000,000.00", image: whitedge },
+  { name: "whitedge duplex", price: "₦75,000,000.00", image: whitedge },
 ];
 
 export const cardInfoIconDetails: {
