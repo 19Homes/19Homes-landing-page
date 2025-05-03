5a3ef04bbd8cf47afb7fb16fe2eb33d1c9f4a7a2
Tom Okafor <tmeka008@gmail.com>
2025-05-04 00:50:37 +0100

    feat(hero section): create and style the hero section component

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,3 +1,8 @@
+import styles from "./styles/styles.module.css";
+import image1 from "../../../public/hero/image1.png";
+import image2 from "../../../public/hero/image2.png";
+import image3 from "../../../public/hero/image3.png";
+import Image from "next/image";
 export default function Hero() {
   return (
     <section className="w-full max-w-[1512px] px-14 grid grid-cols-2 gap-6 mx-auto items-center">
@@ -16,7 +21,33 @@ export default function Hero() {
           Get Started
         </button>
       </div>
-      <div className="bg-gold-50 h-[620px]"></div>
+      <div className={`h-[620px] relative ${styles.imageContainer}`}>
+        <div className={`${styles.imageBox} ${styles.imageBoxOne}`}>
+          <Image
+            src={image1}
+            alt="A sophisticated building"
+            fill
+            placeholder="blur"
+          />
+        </div>
+        <div className={`${styles.imageBox} ${styles.imageBoxTwo}`}>
+          <Image
+            src={image2}
+            alt="A sophisticated building"
+            fill
+            placeholder="blur"
+          />
+        </div>
+        <div className={`${styles.imageBox} ${styles.imageBoxThree}`}>
+          <Image
+            src={image3}
+            alt="A sophisticated building"
+            fill
+            placeholder="blur"
+          />
+        </div>
+        <div className="w-[80%] h-[562px] border-[10px] border-gold-25 rounded-4xl border-dashed absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"></div>
+      </div>
     </section>
   );
 }
