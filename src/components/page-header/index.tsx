01d02f244ec0a9fd2fc5f9e54d3122d524b65503
Tom Okafor <tmeka008@gmail.com>
2025-05-05 02:03:36 +0100

    style: add margin top of 73px to the page header to adhere to the correct desgin implementation

--- src/components/page-header/index.tsx
+++ src/components/page-header/index.tsx
@@ -1,6 +1,6 @@
 export default function PageHeader({ title }: { title: string }) {
   return (
-    <section className="w-full h-[84px] flex justify-center items-center bg-[linear-gradient(to_right,rgba(0,0,0,0.5),rgba(0,0,0,0.9),rgba(0,0,0,0.5)),url('/footer/footer-image.png')] bg-center bg-cover">
+    <section className="w-full h-[84px] flex justify-center items-center bg-[linear-gradient(to_right,rgba(0,0,0,0.5),rgba(0,0,0,0.9),rgba(0,0,0,0.5)),url('/footer/footer-image.png')] bg-center bg-cover mt-[73px]">
       <h3 className="font-poppins capitalize font-bold text-2xl text-gold-100">
         {title}
       </h3>
