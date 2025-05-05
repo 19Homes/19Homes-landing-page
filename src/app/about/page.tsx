eb91fb6f6d12556cba798b34f42b6cd884380c09
Tom Okafor <tmeka008@gmail.com>
2025-05-05 01:42:01 +0100

    feat(about): structure and style the about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,17 @@
 import PageHeader from "@/components/page-header";
+import BlockOne from "./components/block-one";
+import BlockTwo from "./components/block-two";
+import CompanyStatements from "./components/company-statements";
 
 export default function About() {
   return (
-    <main>
+    <main className="flex flex-col items-center">
       <PageHeader title="about us" />
+      <section className="mt-20 flex flex-col gap-[100px] px-14">
+        <BlockOne />
+        <BlockTwo />
+        <CompanyStatements />
+      </section>
     </main>
   );
 }
