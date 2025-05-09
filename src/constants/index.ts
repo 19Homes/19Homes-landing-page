93901c1d2757bf4610b0b5062e44b621c46230d6
Tom Okafor <tmeka008@gmail.com>
2025-05-09 19:11:13 +0100

    feat: create a constant for the company desings holding the id and images for each design

---
+++ src/constants/index.ts
@@ -0,0 +1,59 @@
+import { DesignsType } from "@/types";
+import designOne from "../../public/designs/design.jpg";
+import designOne1 from "../../public/designs/design1.jpg";
+import designOne2 from "../../public/designs/design2.jpg";
+import designTwo from "../../public/designs/design-two.jpg";
+import designTwo1 from "../../public/designs/design-two1.jpg";
+import designTwo2 from "../../public/designs/design-two2.jpg";
+import designThree from "../../public/designs/design-three.jpg";
+import designThree1 from "../../public/designs/design-three1.jpg";
+import designThree2 from "../../public/designs/design-three2.jpg";
+import designFour from "../../public/designs/design-four.jpg";
+import designFour1 from "../../public/designs/design-four1.jpg";
+import designFour2 from "../../public/designs/design-four2.jpg";
+import designFive from "../../public/designs/design-five.jpg";
+import designFive1 from "../../public/designs/design-five1.jpg";
+import designFive2 from "../../public/designs/design-five2.jpg";
+
+export const CompanyDesigns: DesignsType[] = [
+  {
+    id: "001",
+    images: {
+      imageOne: designOne,
+      imageTwo: designOne1,
+      imageThree: designOne2,
+    },
+  },
+  {
+    id: "002",
+    images: {
+      imageOne: designTwo,
+      imageTwo: designTwo1,
+      imageThree: designTwo2,
+    },
+  },
+  {
+    id: "003",
+    images: {
+      imageOne: designThree,
+      imageTwo: designThree1,
+      imageThree: designThree2,
+    },
+  },
+  {
+    id: "004",
+    images: {
+      imageOne: designFour,
+      imageTwo: designFour1,
+      imageThree: designFour2,
+    },
+  },
+  {
+    id: "005",
+    images: {
+      imageOne: designFive,
+      imageTwo: designFive1,
+      imageThree: designFive2,
+    },
+  },
+];
