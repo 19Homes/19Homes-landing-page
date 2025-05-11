188ab5c9d5a3b3619195d1568744501dc2ab22ff
Tom Okafor <tmeka008@gmail.com>
2025-05-11 15:01:44 +0100

    refactor: set array of design images and map through the images to display the image buttons rather than creating a new button image for each new image button

--- src/app/design/[id]/components/design-image-block/index.tsx
+++ src/app/design/[id]/components/design-image-block/index.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 import UseGetCurrentDesign from "@/hooks/useGetCurrentDesign";
 export default function DesignImageBlock() {
   const currentDesign = UseGetCurrentDesign();
+  const images = [
+    currentDesign.images.imageOne,
+    currentDesign.images.imageTwo,
+    currentDesign.images.imageThree,
+  ];
   return (
     <section className="mt-20 flex w-full flex-col items-center gap-12 px-14">
       <div className="relative h-[747px] w-full">
@@ -12,31 +17,20 @@ export default function DesignImageBlock() {
           className="object-cover"
         />
       </div>
-      <div className="*:border-black-100 flex items-center gap-8 *:relative *:h-[200px] *:w-[200px] *:cursor-pointer *:overflow-hidden *:rounded-xl *:border-2 *:shadow-[0_3px_8px_#999] *:hover:shadow-[0_5px_12px_#777] *:duration-300 *:focus:border-gold-75 *:focus:outline-none">
-        <button className="group">
-          <Image
-            src={currentDesign.images.imageOne}
-            alt="first design"
-            fill
-            className="object-cover duration-300 group-hover:scale-[1.1]"
-          />
-        </button>
-        <button className="group">
-          <Image
-            src={currentDesign.images.imageTwo}
-            alt="second design"
-            fill
-            className="object-cover duration-300 group-hover:scale-[1.1]"
-          />
-        </button>
-        <button className="group">
-          <Image
-            src={currentDesign.images.imageThree}
-            alt="third design"
-            fill
-            className="object-cover duration-300 group-hover:scale-[1.1]"
-          />
-        </button>
+      <div className="flex justify-center gap-6">
+        {images.map((image, index) => (
+          <button
+            key={index}
+            className="border-black-100 focus:border-gold-75 group relative h-50 w-50 cursor-pointer overflow-hidden rounded-[10px] border-3 shadow-[0_3px_10px_rgba(0,0,0,0.2)] duration-300 hover:shadow-[0_5px_12px_rgba(0,0,0,0.5)] focus:outline-none"
+          >
+            <Image
+              src={image}
+              alt={`Design ${index}`}
+              fill
+              className="object-cover duration-300 group-hover:scale-[1.1]"
+            />
+          </button>
+        ))}
       </div>
     </section>
   );
