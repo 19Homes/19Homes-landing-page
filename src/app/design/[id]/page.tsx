825040ed687d8ee5d0e3dcc3cf75e1b44c87f64e
Tom Okafor <tmeka008@gmail.com>
2025-05-11 13:43:39 +0100

    feat(design page): create page for individual company design

---
+++ src/app/design/[id]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import PageHeader from "@/components/page-header";
+import DesignImageBlock from "./components/design-image-block";
+export default function Page() {
+  return (
+    <main>
+      <PageHeader title="Single Design Portfolio" />
+      <DesignImageBlock />
+    </main>
+  );
+}
