3d6549d0b9af2471f97fd365871324c6e2d997e0
Tom Okafor <tmeka008@gmail.com>
2025-05-11 18:10:08 +0100

    feat: create useGetOtherDesigns hook for the functionality of reading the id parameter form the url and filtering the designs whose ids do not match the url id.

---
+++ src/hooks/useGetOtherDesigns.tsx
@@ -0,0 +1,8 @@
+import { useParams } from "next/navigation";
+import { CompanyDesigns } from "@/constants";
+
+export default function UseGetOtherDesigns() {
+  const { id } = useParams();
+  const otherDesigns = CompanyDesigns.filter((design) => id !== design.id);
+  return otherDesigns;
+}
