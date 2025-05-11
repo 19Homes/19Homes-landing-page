6ba79e3b01c673d2e805a6f5559f7a63fe91e993
Tom Okafor <tmeka008@gmail.com>
2025-05-11 13:39:36 +0100

    feat: redirect to not found page when there is not matching id for the id in the url

--- src/hooks/useGetCurrentDesign.tsx
+++ src/hooks/useGetCurrentDesign.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { useParams } from "next/navigation";
+import { useParams, notFound } from "next/navigation";
 import { CompanyDesigns } from "@/constants";
 
 export default function UseGetCurrentDesign() {
   const { id } = useParams();
   const currentDesign = CompanyDesigns.find((design) => id === design.id);
+  if (!currentDesign) notFound();
   return currentDesign;
 }
