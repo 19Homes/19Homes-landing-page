b7d20c013061fc1423cf580b9e2f2385db240746
Tom Okafor <tmeka008@gmail.com>
2025-05-12 19:39:48 +0100

    feat: use ActiveLinkContext to manage header and footer nav link states

--- src/components/footer/components/footer-categories-block/index.tsx
+++ src/components/footer/components/footer-categories-block/index.tsx
@@ -1,19 +1,30 @@
+"use client";
 import Link from "next/link";
 import { footerCategories } from "../../constants";
+import { useContext } from "react";
+import { activeLinkContext } from "@/contexts/activeLinkContext";
 
 export default function FooterCategoriesBlock() {
+  const { setActiveLinkIndex } = useContext(activeLinkContext);
   return (
     <section className="space-y-8 capitalize">
-      <h3 className="text-2xl text-white font-poppins font-bold text-center">
+      <h3 className="font-poppins text-center text-2xl font-bold text-white">
         {footerCategories.blockTitle}
       </h3>
       <ul className="space-y-6">
         {footerCategories.items.map((item, index) => (
           <li
             key={index}
-            className="font-montserrat font-medium text-white text-sm text-center hover:text-gold-100 duration-200"
+            className="font-montserrat hover:text-gold-100 text-center text-sm font-medium text-white duration-200"
           >
-            <Link href={item.path}>{item.name}</Link>
+            <Link
+              href={item.path}
+              onClick={() => {
+                setActiveLinkIndex(index);
+              }}
+            >
+              {item.name}
+            </Link>
           </li>
         ))}
       </ul>
--- src/components/header/components/nav/index.tsx
+++ src/components/header/components/nav/index.tsx
@@ -2,14 +2,14 @@
 
 import Link from "next/link";
 import { navigationLinks } from "../../constants";
-import { useState } from "react";
+import { useContext } from "react";
 import { cn } from "@/utils/classnames";
-import UseActiveLinkIndex from "../../hooks/useActiveLink";
 import { ChevronDown } from "lucide-react";
+import { activeLinkContext } from "@/contexts/activeLinkContext";
 export default function Navigation() {
-  const [isActive, setActiveState] = useState<number>(UseActiveLinkIndex());
+  const { setActiveLinkIndex, activeLinkIndex } = useContext(activeLinkContext);
   const handleLinkClick = (index: number) => {
-    setActiveState(index);
+    setActiveLinkIndex(index);
   };
   return (
     <nav className="flex h-fit gap-12">
@@ -19,7 +19,7 @@ export default function Navigation() {
           href={link.path}
           className={cn(
             "font-montserrat group p-2 text-sm font-medium capitalize duration-300",
-            index === isActive ? "text-gold-100" : "text-black-100",
+            index === activeLinkIndex ? "text-gold-100" : "text-black-100",
             link.hasChild ? "hover:bg-gold-25" : "hover:text-gold-75",
           )}
           onClick={() => {
@@ -27,18 +27,20 @@ export default function Navigation() {
           }}
         >
           {link.hasChild ? (
-            <div className="flex flex-col items-center gap-2 h-[74px] group-hover:w-[161px] relative duration-300">
+            <div className="relative flex h-[74px] flex-col items-center gap-2 duration-300 group-hover:w-[161px]">
               <span className="flex items-center gap-2">
                 <span>{link.text}</span>
                 <ChevronDown
                   size={16}
                   className={cn(
                     "duration-300 group-hover:rotate-180",
-                    index === isActive ? "text-gold-100" : "text-[#1C1B1F]",
+                    index === activeLinkIndex
+                      ? "text-gold-100"
+                      : "text-[#1C1B1F]",
                   )}
                 />
               </span>
-              <button className="font-montserrat hidden bg-white p-2 text-sm font-medium text-black group-hover:block cursor-pointer absolute bottom-0 left-2">
+              <button className="font-montserrat absolute bottom-0 left-2 hidden cursor-pointer bg-white p-2 text-sm font-medium text-black group-hover:block">
                 Become an Agent
               </button>
             </div>
