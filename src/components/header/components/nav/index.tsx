5c51a5cf5aa2477dbe2c3cb6ac4d6678950e9809
Tom Okafor <tmeka008@gmail.com>
2025-05-12 15:44:00 +0100

    feat: add become a member dropdown when the career link is hovered

--- src/components/header/components/nav/index.tsx
+++ src/components/header/components/nav/index.tsx
@@ -12,30 +12,36 @@ export default function Navigation() {
     setActiveState(index);
   };
   return (
-    <nav className="flex gap-14 h-fit">
+    <nav className="flex h-fit gap-12">
       {navigationLinks.map((link, index) => (
         <Link
           key={index}
           href={link.path}
           className={cn(
-            "font-montserrat font-medium text-sm capitalize hover:text-gold-75 duration-300 group",
-            index === isActive ? "text-gold-100" : "text-black-100"
+            "font-montserrat group p-2 text-sm font-medium capitalize duration-300",
+            index === isActive ? "text-gold-100" : "text-black-100",
+            link.hasChild ? "hover:bg-gold-25" : "hover:text-gold-75",
           )}
           onClick={() => {
             handleLinkClick(index);
           }}
         >
           {link.hasChild ? (
-            <span className="flex gap-2 items-center">
-              <span>{link.text}</span>
-              <ChevronDown
-                size={16}
-                className={cn(
-                  "group-hover:rotate-180 duration-300 group-hover:text-gold-75",
-                  index === isActive ? "text-gold-100" : "text-[#1C1B1F]"
-                )}
-              />
-            </span>
+            <div className="flex flex-col items-center gap-2 h-[74px] group-hover:w-[161px] relative duration-300">
+              <span className="flex items-center gap-2">
+                <span>{link.text}</span>
+                <ChevronDown
+                  size={16}
+                  className={cn(
+                    "duration-300 group-hover:rotate-180",
+                    index === isActive ? "text-gold-100" : "text-[#1C1B1F]",
+                  )}
+                />
+              </span>
+              <button className="font-montserrat hidden bg-white p-2 text-sm font-medium text-black group-hover:block cursor-pointer absolute bottom-0 left-2">
+                Become an Agent
+              </button>
+            </div>
           ) : (
             link.text
           )}
