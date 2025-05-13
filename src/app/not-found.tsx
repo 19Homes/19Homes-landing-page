f8f88df8afbe1adf5fc36b92a792505410aca8d9
Tom Okafor <tmeka008@gmail.com>
2025-05-13 19:31:50 +0100

    feat(not-found): add 404 text and button to route back to the home page to the not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,19 @@
+"use client";
 import image from "../../public/not-found/not-found.jpg";
 import image1 from "../../public/not-found/not-found1.jpg";
 import image2 from "../../public/not-found/not-found2.jpg";
 import image3 from "../../public/not-found/not-found3.jpg";
 import image4 from "../../public/not-found/not-found4.jpg";
 import Image from "next/image";
+import Button from "@/components/button";
+import { useRouter } from "next/navigation";
+import { useContext } from "react";
+import { activeLinkContext } from "@/contexts/activeLinkContext";
 export default function NotFound() {
+  const router = useRouter();
+  const { setActiveLinkIndex } = useContext(activeLinkContext);
   return (
-    <main className="flex w-full items-center justify-between gap-12 px-14">
+    <main className="flex w-full items-center gap-12 px-14">
       <div className="grid h-[500px] w-[60%] grid-cols-3 grid-rows-4 gap-2 overflow-hidden rounded-3xl *:overflow-hidden *:rounded-3xl">
         <div className="bg-gold-75 group relative col-start-1 col-end-2 row-span-4">
           <Image
@@ -53,6 +60,21 @@ export default function NotFound() {
             placeholder="blur"
           />
         </div>
+      </div>
+      <div className="flex w-[40%] flex-col items-center gap-8">
+        <h2 className="font-montserrat text-black-100 text-9xl font-bold">
+          4 <span className="text-gold-100">0</span> 4
+        </h2>
+        <p className="font-montserrat text-black-100 text-3xl font-medium">
+          Sorry, this page does not exist.
+        </p>
+        <Button
+          text="Return Home"
+          onclick={() => {
+            router.push("/");
+            setActiveLinkIndex(0);
+          }}
+        />
       </div>
     </main>
   );
