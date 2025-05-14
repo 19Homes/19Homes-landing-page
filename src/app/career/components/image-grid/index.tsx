530300e531b13016ede4f5ed170b438059f0350e
Tom Okafor <tmeka008@gmail.com>
2025-05-14 22:49:07 +0100

    feat: structure and style the image grid component

--- src/app/career/components/image-grid/index.tsx
+++ src/app/career/components/image-grid/index.tsx
@@ -1,5 +1,6 @@
-import { centralImages, column1Images, column3Images } from "../../constants";
-import Image, { StaticImageData } from "next/image";
+import { column1Images, column3Images } from "../../constants";
+import CardColumn from "../card-column";
+import CentralImage from "../central-image";
 
 export default function ImageGrid() {
   return (
@@ -7,45 +8,10 @@ export default function ImageGrid() {
       <div className="animate-card-slide flex h-full flex-col gap-4">
         <CardColumn columnImages={column1Images} />
       </div>
-      <div className="bg-gold-25 relative col-span-3 h-[500px] overflow-hidden rounded-xl">
-        <Image
-          alt="building"
-          src={centralImages[0]}
-          fill
-          className="object-cover"
-          placeholder="blur"
-        />
-      </div>
+      <CentralImage />
       <div className="animate-card-slide-down flex h-full translate-y-[-50.5%] flex-col gap-4 overflow-y-hidden">
         <CardColumn columnImages={column3Images} />
       </div>
     </div>
   );
 }
-
-const CardColumn = ({ columnImages }: { columnImages: StaticImageData[] }) => {
-  return (
-    <>
-      {Array(2)
-        .fill("")
-        .map((_, index) => (
-          <div key={index} className="flex flex-col gap-4">
-            {columnImages.map((image, index) => (
-              <div
-                key={index}
-                className="bg-black-25 relative h-[110px] w-full shrink-0 overflow-hidden rounded-xl"
-              >
-                <Image
-                  src={image}
-                  alt="a beautiful apartment"
-                  fill
-                  className="object-cover"
-                  placeholder="blur"
-                />
-              </div>
-            ))}
-          </div>
-        ))}
-    </>
-  );
-};
