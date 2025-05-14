3ba8ebeb7c3195cfbbbd58599bb5a5f83673140b
Tom Okafor <tmeka008@gmail.com>
2025-05-14 17:30:07 +0100

    import images needed for the career page and export them in groups of arrays as needed in the page for the grid image gallery

---
+++ src/app/career/constants/index.ts
@@ -0,0 +1,19 @@
+import image from "../../../../public/not-found/not-found.jpg";
+import image1 from "../../../../public/not-found/not-found1.jpg";
+import image2 from "../../../../public/not-found/not-found2.jpg";
+import image3 from "../../../../public/not-found/not-found3.jpg";
+import image4 from "../../../../public/not-found/not-found4.jpg";
+import image5 from "../../../../public/about/background.png";
+import image6 from "../../../../public/about/block-one-image.jpg";
+import image7 from "../../../../public/about/block-one-image1.jpg";
+import image8 from "../../../../public/about/block-two-image.jpg";
+import image9 from "../../../../public/about/block-two-image1.jpg";
+import image10 from "../../../../public/featured-properties/one story building.png";
+import image11 from "../../../../public/designs/design.jpg";
+import image12 from "../../../../public/designs/design-three2.jpg";
+import image13 from "../../../../public/designs/design-five.jpg";
+import image14 from "../../../../public/designs/design-four1.jpg";
+
+export const centralImages = [image10, image11, image12, image13, image14];
+export const column1Images = [image, image1, image2, image3, image4];
+export const column3Images = [image5, image6, image7, image8, image9];
