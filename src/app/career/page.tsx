0fc147eda10dc1b62c2257a0eabed039df96ef40
Tom Okafor <tmeka008@gmail.com>
2025-05-14 23:11:59 +0100

    feat: add main text to career page inviting people to become agents with 19Homes

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -4,8 +4,23 @@ export default function Page() {
   return (
     <main>
       <PageHeader title="become an agent" />
-      <section className="mt-20 flex w-full max-w-[1512px] gap-8 px-14">
+      <section className="mt-20 flex w-full max-w-[1512px] items-center gap-8 px-14">
         <ImageGrid />
+        <div className="flex flex-col gap-4">
+          <h2 className="text-black-100 font-poppins text-7xl font-bold uppercase">
+            build <br />
+            <span className="text-gold-100">a community</span>
+            <br /> with 19homes
+          </h2>
+          <p className="font-montserrat text-black-75 text-2xl font-normal">
+            Become an agent with 19Homes and help us reach more people. We are a
+            fast growing community and your progress and success are assured
+            with us.
+          </p>
+          <p className="font-montserrat text-gold-100 text-xl font-bold uppercase">
+            kindly fill the form below!
+          </p>
+        </div>
       </section>
     </main>
   );
