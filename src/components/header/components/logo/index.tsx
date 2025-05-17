191faed93b3ddf51e0df19d9bb9f67f59d8a26c5
Tom Okafor <tmeka008@gmail.com>
2025-05-17 22:32:43 +0100

    feat: add new logo icon for the mobile header section

--- src/components/header/components/logo/index.tsx
+++ src/components/header/components/logo/index.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
-
+import logo from "../../../../../public/19Homes-icon.png";
 export default function Logo() {
   return (
-    <Image
-      src="/19Homes-icon.svg"
-      height={56}
-      width={142}
-      alt="19 Homes Logo"
-    />
+    <div>
+      <Image
+        src="/19Homes-icon.svg"
+        height={56}
+        width={142}
+        alt="19 Homes Logo"
+        className="hidden lg:block"
+      />
+      <div className="relative h-6 w-7.5">
+        <Image fill src={logo} alt="19Homes logo" className="lg:hidden" />
+      </div>
+    </div>
   );
 }
