157058a6c3fc804dcd9dcf99930498c82908fdb1
Tom Okafor <tmeka008@gmail.com>
2025-05-19 20:40:05 +0100

    fix(features): correct width of features card for larger devices

--- src/components/features/components/feature-card/index.tsx
+++ src/components/features/components/feature-card/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function FeatureCard({ title, description, image }: CardBase) {
   return (
-    <article className="flex w-full max-w-[343px] shrink-0 flex-col items-center justify-center gap-6 lg:gap-8 rounded-4xl p-4.5 shadow-[0_4px_4px_rgba(0,0,0,0.25)] lg:w-[450px] lg:p-6">
+    <article className="flex w-full max-w-[343px] shrink-0 flex-col items-center justify-center gap-6 rounded-4xl p-4.5 shadow-[0_4px_4px_rgba(0,0,0,0.25)] lg:w-[450px] lg:max-w-[450px] lg:gap-8 lg:p-6">
       <div className="flex w-full flex-col items-center gap-4.5 lg:gap-6">
         <Image
           src={image}
