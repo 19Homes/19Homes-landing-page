791f4eff448aafb6b50c4f8259385f1c0a46528e
Tom Okafor <tmeka008@gmail.com>
2025-05-19 20:40:37 +0100

    feat: add responsive design to button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -10,7 +10,7 @@ export default function Button({
 }) {
   return (
     <button
-      className="border-gold-100 text-gold-100 font-montserrat hover:bg-gold-100 flex items-center justify-center gap-3.5 rounded-sm border bg-white px-6 py-4 text-sm font-bold capitalize duration-200 hover:text-white hover:shadow-[3px_5px_10px_#0000007f]"
+      className="border-gold-100 text-gold-100 font-montserrat hover:bg-gold-100 flex items-center justify-center gap-3.5 rounded-sm border bg-white px-4.5 py-3 text-[10px] font-bold capitalize duration-200 hover:text-white hover:shadow-[3px_5px_10px_#0000007f] lg:px-6 lg:py-4 lg:text-sm"
       onClick={onclick}
     >
       <span>{text}</span>
