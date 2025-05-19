cd5f51b83f56e30f3e8fecc87274c83585323950
Tom Okafor <tmeka008@gmail.com>
2025-05-19 20:07:07 +0100

    refactor: adjust styles for hero section grid to improve responsiveness

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -6,9 +6,9 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <section className="mx-auto flex w-full max-w-[1512px] flex-col items-center gap-10 px-6 lg:grid lg:grid-cols-2 lg:gap-6 lg:px-14">
-      <div className="flex w-full flex-col items-center lg:items-start gap-8">
-        <div className="flex flex-col items-center lg:items-start gap-6">
-          <h1 className="font-poppins text-black-100 md:leading-[80px] max-w-prose text-center text-5xl leading-[51px] font-bold tracking-[4%] uppercase md:text-[64px] lg:text-left">
+      <div className="flex w-full flex-col items-center gap-8 lg:items-start">
+        <div className="flex flex-col items-center gap-6 lg:items-start">
+          <h1 className="font-poppins text-black-100 max-w-prose text-center text-5xl leading-[51px] font-bold tracking-[4%] uppercase md:text-[64px] md:leading-[80px] lg:text-left">
             your <span className="text-gold-100">perfect home</span> is our goal
           </h1>
           <p className="font-montserrat text-black-100 max-w-[40ch] text-center text-xl font-medium lg:text-left">
@@ -21,7 +21,9 @@ export default function Hero() {
           Get Started
         </button>
       </div>
-      <div className={`relative h-[308px] lg:h-[620px] w-full ${styles.imageContainer}`}>
+      <div
+        className={`relative h-[308px] w-full lg:h-[620px] ${styles.imageContainer}`}
+      >
         <div className={`${styles.imageBox} ${styles.imageBoxOne}`}>
           <Image
             src={image1}
@@ -46,7 +48,7 @@ export default function Hero() {
             placeholder="blur"
           />
         </div>
-        <div className="border-gold-25 absolute top-[50%] left-[50%] h-[279px] w-[273px] translate-x-[-50%] translate-y-[-50%] rounded-[15px] border-[5px] border-dashed"></div>
+        <div className="border-gold-25 absolute top-[50%] left-[50%] h-[279px] w-[273px] translate-x-[-50%] translate-y-[-50%] rounded-[15px] border-[5px] border-dashed lg:h-[562px] lg:w-[551px] lg:rounded-4xl lg:border-10"></div>
       </div>
     </section>
   );
