cc5377966bbd9e7e9204e692d58d8ab8a768be94
Tom Okafor <tmeka008@gmail.com>
2025-05-19 19:31:32 +0100

    feat: add responsive design to the features section

--- src/components/features/components/feature-card/index.tsx
+++ src/components/features/components/feature-card/index.tsx
@@ -1,22 +1,23 @@
-import { CardBase } from "@/types";
+import type { CardBase } from "@/types";
 import Image from "next/image";
 
 export default function FeatureCard({ title, description, image }: CardBase) {
   return (
-    <article className="shrink-0 flex flex-col items-center justify-center gap-8 rounded-4xl p-6 w-[450px] shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
-      <div className="w-full flex flex-col gap-6 items-center">
+    <article className="flex w-full max-w-[343px] shrink-0 flex-col items-center justify-center gap-6 lg:gap-8 rounded-4xl p-4.5 shadow-[0_4px_4px_rgba(0,0,0,0.25)] lg:w-[450px] lg:p-6">
+      <div className="flex w-full flex-col items-center gap-4.5 lg:gap-6">
         <Image
           src={image}
           alt={`${title} icon`}
           aria-hidden="true"
           width={50}
           height={50}
+          className="size-[38px] lg:size-[50px]"
         />
-        <h2 className="capitalize text-center text-3xl text-black-100 font-poppins font-semibold">
+        <h2 className="text-black-100 font-poppins text-center text-[22px] font-semibold capitalize lg:text-3xl">
           {title}
         </h2>
       </div>
-      <p className="text-xl text-black-100 font-montserrat font-regular text-center">
+      <p className="text-black-100 font-montserrat font-regular text-center text-[1rem] lg:text-xl">
         {description}
       </p>
     </article>
--- src/components/features/index.tsx
+++ src/components/features/index.tsx
@@ -3,11 +3,11 @@ import FeatureCard from "./components/feature-card";
 
 export default function Features() {
   return (
-    <section className="w-full  flex flex-col gap-20 items-center">
-      <h2 className="font-poppins font-bold text-3xl text-black100 text-center capitalize">
+    <section className="flex w-full flex-col items-center gap-10 lg:gap-20">
+      <h2 className="font-poppins text-black-100 text-center text-xl font-bold capitalize lg:text-3xl">
         why <span className="text-gold-100">19Homes</span>
       </h2>
-      <section className="w-full max-w-[1512px] px-14 flex items-center justify-center flex-wrap gap-y-10 gap-[25px]">
+      <section className="flex w-full max-w-[1512px] flex-wrap items-center justify-center gap-[25px] gap-y-5 px-6 lg:gap-y-10 lg:px-14">
         {featuresData.map((feature, index) => (
           <FeatureCard
             key={index}
