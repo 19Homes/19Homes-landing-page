56c6e490117ae81e1f6866b1eb9a3a988bbb3a31
Tom Okafor <tmeka008@gmail.com>
2025-05-19 18:45:41 +0100

    fix(hero): align hero text items to start in desktop devices to fix layout issue

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -6,9 +6,9 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <section className="mx-auto flex w-full max-w-[1512px] flex-col items-center gap-10 px-6 lg:grid lg:grid-cols-2 lg:gap-6 lg:px-14">
-      <div className="flex w-full flex-col items-center gap-8">
-        <div className="flex flex-col items-center gap-6">
-          <h1 className="font-poppins text-black-100 md:leadind-[80px] max-w-prose text-center text-5xl leading-[51px] font-bold tracking-[4%] uppercase md:text-[64px] lg:text-left">
+      <div className="flex w-full flex-col items-center lg:items-start gap-8">
+        <div className="flex flex-col items-center lg:items-start gap-6">
+          <h1 className="font-poppins text-black-100 md:leading-[80px] max-w-prose text-center text-5xl leading-[51px] font-bold tracking-[4%] uppercase md:text-[64px] lg:text-left">
             your <span className="text-gold-100">perfect home</span> is our goal
           </h1>
           <p className="font-montserrat text-black-100 max-w-[40ch] text-center text-xl font-medium lg:text-left">
