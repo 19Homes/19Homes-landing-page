64927418009c137099474ca282863715e9b4faa7
Tom Okafor <tmeka008@gmail.com>
2025-05-23 23:29:32 +0100

    feat: add responsive design to career page form

--- src/app/career/components/form/index.tsx
+++ src/app/career/components/form/index.tsx
@@ -2,11 +2,11 @@
 import { cn } from "@/utils/classnames";
 import Button from "@/components/button";
 const styleClasses =
-  "border p-4 rounded-sm border-black-100 placeholder:font-montserrat font-montserrat placeholder:font-normal font-medium placeholder:text-black-75 text-black-100 text-sm placeholder:text-sm w-[395px]";
+  "border p-4 rounded-sm border-black-100 placeholder:font-montserrat font-montserrat placeholder:font-normal font-medium placeholder:text-black-75 text-black-100 text-sm placeholder:text-sm w-[min(100%,365px)]";
 
 export default function CareerForm() {
   return (
-    <form className="flex w-[806px] flex-col items-center gap-6">
+    <form className="flex w-[min(100%,806px)] flex-col items-center gap-6 px-6">
       <section className="flex flex-wrap items-center justify-center gap-x-4 gap-y-6">
         <input
           type="text"
@@ -39,7 +39,9 @@ export default function CareerForm() {
           name="inquiries"
           id=""
           placeholder="Please, let us know if you have any questions or enquiries."
-          className={cn(`resize-none ${styleClasses} min-h-[250px] w-[488px]`)}
+          className={cn(
+            `resize-none ${styleClasses} min-h-[250px] w-[min(365px,100%)]`,
+          )}
         ></textarea>
       </section>
       <Button text="JOIN 19HOMES" onclick={() => {}} />
