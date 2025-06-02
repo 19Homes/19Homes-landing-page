fea2f6e6d4c01521cef7acf26c1d60c93c67f928
Tom Okafor <tmeka008@gmail.com>
2025-06-03 00:06:46 +0100

    feat: use RegisterFormSchema to validate the register form

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,8 +1,19 @@
 "use server";
-
+import { RegisterFormSchema } from "@/lib/rules";
 export async function register(_: unknown, formData: FormData) {
   const email = formData.get("email");
   const name = formData.get("name");
   const password = formData.get("password");
-  console.log(email, name, password);
+  const validatedFields = RegisterFormSchema.safeParse({
+    email: email,
+    name: name,
+    password: password,
+    confirmPassword: formData.get("confirm_password"),
+  });
+  if (!validatedFields.success) {
+    console.log(validatedFields.error.flatten().fieldErrors);
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
 }
