ec1f01a9f5b4bdc9deee4fe32d4e0089c34b330b
Tom Okafor <tmeka008@gmail.com>
2025-06-03 00:31:45 +0100

    feat: return graceful error messages for any field errrors

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -15,38 +15,91 @@ export default function Page() {
       <section className="mt-13 flex w-full flex-col items-center px-6 md:px-14 lg:mt-25">
         <form
           action={action}
-          className="flex w-[min(100%,806px)] flex-col items-center gap-6 px-6"
+          className="flex w-[min(100%,450px)] flex-col items-center gap-6 px-6"
         >
           <section className="flex w-full flex-col items-center justify-center gap-6">
-            <input
-              type="text"
-              placeholder="Your Name"
-              name="name"
-              required
-              className={styleClasses}
-            />
-            <input
-              type="email"
-              placeholder="Your Email"
-              name="email"
-              required
-              className={styleClasses}
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="Your Password"
-              required
-              className={styleClasses}
-            />
-            <input
-              type="password"
-              name="confirm_password"
-              placeholder="Confirm Password"
-              className={styleClasses}
-            />
+            <div className="flex w-full flex-col gap-2">
+              <input
+                type="text"
+                placeholder="Your Name"
+                name="name"
+                required
+                className={styleClasses}
+              />
+              {state?.errors?.name && (
+                <p
+                  className="font-montserrat font-regular text-left text-sm font-bold text-red-500"
+                  aria-live="assertive"
+                  role="alert"
+                >
+                  {state.errors.name}
+                </p>
+              )}
+            </div>
+
+            <div className="flex w-full flex-col gap-2">
+              <input
+                type="email"
+                placeholder="Your Email"
+                name="email"
+                required
+                className={styleClasses}
+                defaultValue={
+                  typeof state?.email === "string" ? state.email : ""
+                }
+              />
+              {state?.errors?.email && (
+                <p
+                  className="font-montserrat font-regular text-left text-sm font-bold text-red-500"
+                  aria-live="assertive"
+                  role="alert"
+                >
+                  {state.errors.email}
+                </p>
+              )}
+            </div>
+
+            <div className="flex w-full flex-col gap-2">
+              <input
+                type="password"
+                name="password"
+                placeholder="Your Password"
+                required
+                className={styleClasses}
+              />
+              {state?.errors?.password && (
+                <p
+                  className="font-montserrat font-regular text-left text-sm font-bold text-red-500"
+                  aria-live="assertive"
+                  role="alert"
+                >
+                  {state.errors.password}
+                </p>
+              )}
+            </div>
+
+            <div className="flex w-full flex-col gap-2">
+              <input
+                type="password"
+                name="confirm_password"
+                placeholder="Confirm Password"
+                className={styleClasses}
+              />
+              {state?.errors?.confirmPassword && (
+                <p
+                  className="font-montserrat font-regular text-left text-sm font-bold text-red-500"
+                  aria-live="assertive"
+                  role="alert"
+                >
+                  {state.errors.confirmPassword}
+                </p>
+              )}
+            </div>
           </section>
-          <Button text={isPending ? 'Registering' : 'JOIN 19HOMES'} onclick={() => {}} />
+          <Button
+            text={isPending ? "Registering" : "JOIN 19HOMES"}
+            onclick={() => {}}
+          />
         </form>
       </section>
     </main>
