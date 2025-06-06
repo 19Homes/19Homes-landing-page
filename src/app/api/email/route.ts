8d15522adf555eb680e7646dfad31fd8128a804d
Tom Okafor <tmeka008@gmail.com>
2025-06-06 17:11:07 +0100

    refactor: use request body to get firstname and recepient email

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -5,13 +5,15 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_KEY);
 export async function POST(request: NextRequest) {
   try {
-    const requestBody = await request.json();
+    const requestBody: { name: string; email: string } = await request.json();
     console.log(requestBody);
+    const recipientEmail = requestBody.email;
+    const name = requestBody.name;
     const { data, error } = await resend.emails.send({
       from: "Acme <onboarding@resend.dev>",
-      to: ["tmeka008@gmail.com"],
-      subject: "Hello Notes",
-      react: EmailTemplate({ firstname: "John" }),
+      to: [recipientEmail],
+      subject: "Welcome to 19HOMES",
+      react: EmailTemplate({ firstname: name }),
     });
 
     if (error) {
