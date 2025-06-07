f5b547a71af1081ca3617ecc1a178de88677601e
Tom Okafor <tmeka008@gmail.com>
2025-06-07 14:13:18 +0100

    feat: implement database transactions in the register user authentication flow. Add unregistered users and send graceful error messages to already registered users.

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,5 +1,9 @@
 "use server";
 import { RegisterFormSchema } from "@/lib/rules";
+import bcrypt from "bcryptjs";
+import { getCollection } from "@/lib/db";
+import { createSession } from "@/lib/session";
+import { redirect } from "next/navigation";
 export async function register(_: unknown, formData: FormData) {
   const email = formData.get("email");
   const name = formData.get("name");
@@ -17,4 +21,19 @@ export async function register(_: unknown, formData: FormData) {
       email: email,
     };
   }
+  const usersCollection = await getCollection("users");
+  if (!usersCollection)
+    return { errors: { email: "Server Error. Please, try again" } };
+  const existingUser = await usersCollection.findOne({ email });
+  if (existingUser)
+    return {
+      errors: { email: "Email has already been registered. Please sign in." },
+    };
+  const hashedPassword = await bcrypt.hash(validatedFields.data.password, 10);
+  const result = await usersCollection.insertOne({
+    email,
+    password: hashedPassword,
+  });
+  createSession(result.insertedId.toString());
+  redirect("/");
 }
