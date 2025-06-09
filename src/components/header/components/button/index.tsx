49f61ea22b16622048d3c8cd39b7dfc7a94e0ce6
Tom Okafor <tmeka008@gmail.com>
2025-06-09 20:48:48 +0100

    feat: add path prop to button

--- src/components/header/components/button/index.tsx
+++ src/components/header/components/button/index.tsx
@@ -1,13 +1,22 @@
+"use client";
 import { headerButtonTypes } from "../../types";
 import { cn } from "@/utils/classnames";
+import { redirect } from "next/navigation";
 
-export default function HeaderButton({ classnames, text }: headerButtonTypes) {
+export default function HeaderButton({
+  classnames,
+  text,
+  path,
+}: headerButtonTypes) {
   return (
     <button
       className={cn(
         classnames,
-        "p-4 rounded-sm text-sm font-bold font-montserrat capitalize"
+        "font-montserrat cursor-pointer rounded-sm p-4 text-sm font-bold capitalize duration-300 hover:shadow-[3px_5px_10px_#0000007f]",
       )}
+      onClick={() => {
+        redirect(path);
+      }}
     >
       {text}
     </button>
