3118f0f20fd5c8b728e9379278f7222b2815f571
Tom Okafor <tmeka008@gmail.com>
2025-06-09 20:14:20 +0100

    feat: add auth flow for login

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,9 +1,10 @@
 "use server";
-import { RegisterFormSchema } from "@/lib/rules";
+import { RegisterFormSchema, LoginFormSchema } from "@/lib/rules";
 import bcrypt from "bcryptjs";
 import { getCollection } from "@/lib/db";
-import { createSession } from "@/lib/session";
 import { redirect } from "next/navigation";
+import { createSession } from "./session";
+import { use } from "react";
 export async function register(_: unknown, formData: FormData) {
   const email = formData.get("email");
   const name = formData.get("name");
@@ -34,6 +35,39 @@ export async function register(_: unknown, formData: FormData) {
     email,
     password: hashedPassword,
   });
-  createSession(result.insertedId.toString());
+  await createSession(result.insertedId.toString());
+  redirect("/");
+}
+
+export async function login(_: unknown, formData: FormData) {
+  const inputedEmail = formData.get("email");
+  const inputedPassword = formData.get("password");
+  const validatedLoginFields = LoginFormSchema.safeParse({
+    email: inputedEmail,
+    password: inputedPassword,
+  });
+  if (!validatedLoginFields.success) {
+    return {
+      errors: validatedLoginFields.error.flatten().fieldErrors,
+    };
+  }
+  const usersCollection = await getCollection("users");
+  if (!usersCollection)
+    return { errors: { email: "Server Error. Please, try again" } };
+  const existingUser = await usersCollection.findOne({
+    email: validatedLoginFields.data.email,
+  });
+  if (!existingUser)
+    return {
+      errors: { email: "This email has not been registered. Please, sign up." },
+    };
+  const correctPassword = await bcrypt.compare(
+    validatedLoginFields.data.password,
+    existingUser.password,
+  );
+  console.log(correctPassword);
+  if (!correctPassword)
+    return { errors: { password: "You have entered an incorrect password." } };
+  await createSession(existingUser._id.toString());
   redirect("/");
 }
