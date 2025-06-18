7cf548b41306e08fe8b15dbcc12ccbec5a4a88d6
Tom Okafor <tmeka008@gmail.com>
2025-06-18 10:25:46 +0100

    add logout button in place of the sign in and log in button if the user has already been authenticated

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,23 +2,36 @@ import Logo from "./components/logo";
 import Navigation from "./components/nav";
 import HeaderButton from "./components/button";
 import MenuButton from "./components/menu-button";
+import { getAuthUser } from "@/lib/getAuthUser";
 
-export default function Header() {
+export default async function Header() {
+  const authenticatedUser = await getAuthUser();
   return (
     <header className="mt-13 flex items-start justify-between px-6 lg:mt-10 lg:px-14">
       <Logo />
       <Navigation />
       <div className="hidden gap-4 lg:flex">
-        <HeaderButton
-          classnames="border border-gold-100 text-gold-100 bg-white"
-          text="login"
-          path="/login"
-        />
-        <HeaderButton
-          classnames="bg-gold-100 text-white"
-          text="sign up"
-          path="/register"
-        />
+        {authenticatedUser ? (
+          <HeaderButton
+            classnames="bg-gold-100 text-white"
+            text="logout"
+            path="/"
+          />
+        ) : (
+          <>
+            {" "}
+            <HeaderButton
+              classnames="border border-gold-100 text-gold-100 bg-white"
+              text="login"
+              path="/login"
+            />
+            <HeaderButton
+              classnames="bg-gold-100 text-white"
+              text="sign up"
+              path="/register"
+            />
+          </>
+        )}
       </div>
       <MenuButton />
     </header>
