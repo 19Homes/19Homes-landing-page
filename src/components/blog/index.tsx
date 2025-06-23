34d15ddaf0dd93f8e4dd88a834e2a532f3f721e3
Tom Okafor <tmeka008@gmail.com>
2025-06-23 18:49:10 +0100

    style: add shadow to button on hover and use a less sharp color for the button background

--- src/components/blog/index.tsx
+++ src/components/blog/index.tsx
@@ -4,10 +4,10 @@ import SectionCaption from "../sectionCaption";
 
 export default function Blog() {
   return (
-    <section className="flex flex-col px-6 lg:px-14 gap-10 items-center">
+    <section className="flex flex-col items-center gap-10 px-6 lg:px-14">
       <section className="w-full flex-col items-center">
         <SectionCaption headText="Our Blog" subText="See our latest articles" />
-        <section className="w-full flex gap-6 gap-y-4 lg:gap-y-10 flex-wrap justify-center">
+        <section className="flex w-full flex-wrap justify-center gap-6 gap-y-4 lg:gap-y-10">
           {blogPosts.map((post, index) => (
             <BlogCard
               key={index}
@@ -18,7 +18,7 @@ export default function Blog() {
           ))}
         </section>
       </section>
-      <button className="capitalize py-4 px-6 bg-gold-100 text-white rounded-sm cursor-pointer hover:shadow-[0_0_30px_#D49100CC,0_0_30px_#D49100CC,0_0_30px_#D49100CC] font-medium font-montserrat text-sm duration-200">
+      <button className="bg-gold-100 font-montserrat hover:bg-gold-75 hover:text-black-100 cursor-pointer rounded-sm px-6 py-4 text-sm font-bold text-white capitalize duration-200 hover:shadow-[3px_5px_10px_#0000007f]">
         view all
       </button>
     </section>
