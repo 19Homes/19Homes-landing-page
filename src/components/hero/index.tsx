92153701a2f11f754de2ffd6dd5f8b6f20164063
Tom Okafor <tmeka008@gmail.com>
2025-06-23 18:50:21 +0100

    feat: redirect users to register page when get started button is clicked

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,9 +1,14 @@
+'use client'
 import styles from "./styles/styles.module.css";
 import image1 from "../../../public/hero/image1.png";
 import image2 from "../../../public/hero/image2.png";
 import image3 from "../../../public/hero/image3.png";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 export default function Hero() {
+  function redirectToRegister() {
+    redirect("/register");
+  }
   return (
     <section className="mx-auto flex w-full max-w-[1512px] flex-col items-center gap-10 px-6 lg:grid lg:grid-cols-2 lg:gap-6 lg:px-14">
       <div className="flex w-full flex-col items-center gap-8 lg:items-start">
@@ -17,7 +22,10 @@ export default function Hero() {
             Into Reality.
           </p>
         </div>
-        <button className="bg-gold-100 font-montserrat hover:bg-gold-75 hover:text-black-100 w-fit cursor-pointer rounded-sm px-6 py-4 text-sm font-bold text-white transition duration-300">
+        <button
+          className="bg-gold-100 font-montserrat hover:bg-gold-75 hover:text-black-100 w-fit cursor-pointer rounded-sm px-6 py-4 text-sm font-bold text-white transition duration-300 hover:shadow-[3px_5px_10px_#0000007f]"
+          onClick={redirectToRegister}
+        >
           Get Started
         </button>
       </div>
