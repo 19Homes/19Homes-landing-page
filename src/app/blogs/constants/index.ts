4fff6285016560737d79050a5f2349edb69e5c60
Tom Okafor <tmeka008@gmail.com>
2025-06-26 00:38:32 +0100

    feat: create data constant for the blogs page including blog images, title, description and text

---
+++ src/app/blogs/constants/index.ts
@@ -0,0 +1,52 @@
+import { BlogPost } from "../types";
+import image1 from "../../../../public/blog-section/blog-post.png";
+import image2 from "../../../../public/blog-section/blog-post2.png";
+import image3 from "../../../../public/blog-section/blog-post3.png";
+import image4 from "../../../../public/about/block-two-image2.jpg";
+import image5 from "../../../../public/featured-properties/whiteedge duplex.png";
+import image6 from "../../../../public/designs/design-four2.jpg";
+
+export const blogPosts: BlogPost[] = [
+  {
+    title: "real estate investment benefits",
+    image: image1,
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Et aliquam proin pulvinar enim senectus vitae quis aliquam. Pharetra nunc eget viverra magna blandit. Eget dictum sit netus at.",
+    content: "",
+  },
+  {
+    title: "the future of real estate",
+    image: image2,
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Et aliquam proin pulvinar enim senectus vitae quis aliquam. Pharetra nunc eget viverra magna blandit. Eget dictum sit netus at.",
+    content: "",
+  },
+  {
+    title: "why you should invest in real estate",
+    image: image3,
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Et aliquam proin pulvinar enim senectus vitae quis aliquam. Pharetra nunc eget viverra magna blandit. Eget dictum sit netus at.",
+    content: "",
+  },
+  {
+    title: "why you should invest in real estate",
+    image: image4,
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Et aliquam proin pulvinar enim senectus vitae quis aliquam. Pharetra nunc eget viverra magna blandit. Eget dictum sit netus at.",
+    content: "",
+  },
+  {
+    title: "why you should invest in real estate",
+    image: image5,
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Et aliquam proin pulvinar enim senectus vitae quis aliquam. Pharetra nunc eget viverra magna blandit. Eget dictum sit netus at.",
+    content: "",
+  },
+  {
+    title: "why you should invest in real estate",
+    image: image6,
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Et aliquam proin pulvinar enim senectus vitae quis aliquam. Pharetra nunc eget viverra magna blandit. Eget dictum sit netus at.",
+    content: "",
+  },
+];
