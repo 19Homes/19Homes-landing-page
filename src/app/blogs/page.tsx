73045c7c942cdff76370a208c6cafd2a85f40fe0
Tom Okafor <tmeka008@gmail.com>
2025-06-26 00:40:26 +0100

    feat: style and structure the blog cards section of the blogs page

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,8 +1,25 @@
 import PageHeader from "@/components/page-header";
+import BlogPageCard from "./components/blogPageCard";
+import { blogPosts } from "./constants";
 export default function Page() {
   return (
-    <main>
+    <main className="flex flex-col items-center">
       <PageHeader title="Blogs" />
+      <section className="mt-20 w-full max-w-[1510px] flex flex-wrap gap-12 justify-center px-13 md:mt-25 md:px-16">
+        {blogPosts.map((post, index) => (
+          <BlogPageCard
+            key={index}
+            image={post.image}
+            title={post.title}
+            text={cutOutText(post.description)}
+          />
+        ))}
+      </section>
     </main>
   );
+}
+
+function cutOutText(text: string) {
+  const extractedText = `${text.substring(0, 21)}...`;
+  return extractedText;
 }
