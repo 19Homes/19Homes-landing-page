9fe90b1b9f164d71ff4b1720164f64fb8a45801d
Tom Okafor <tmeka008@gmail.com>
2025-06-26 01:13:13 +0100

    feat: redirect to individual blog page on blog card click

--- src/app/blogs/components/blogPageCard/index.tsx
+++ src/app/blogs/components/blogPageCard/index.tsx
@@ -1,4 +1,8 @@
+"use client";
+import { redirect } from "next/navigation";
 import Image, { StaticImageData } from "next/image";
+import { createUniqueBlogPath } from "../../utils/create-unique-blog-path";
+import { useCallback } from "react";
 export default function BlogPageCard({
   image,
   title,
@@ -8,18 +12,24 @@ export default function BlogPageCard({
   title: string;
   text: string;
 }) {
+  const handleButtonClick = useCallback(() => {
+    redirect(`/blog/${createUniqueBlogPath(title)}`);
+  }, [title]);
   return (
-    <button className="group outline-gold-75 relative shrink-0 overflow-hidden rounded-3xl shadow-xl outline-2 outline-offset-8 duration-300 hover:shadow-2xl cursor-pointer">
+    <button
+      className="group outline-gold-75 relative shrink-0 cursor-pointer overflow-hidden rounded-3xl shadow-xl outline-2 outline-offset-8 duration-300 hover:shadow-2xl"
+      onClick={handleButtonClick}
+    >
       <Image
         src={image}
         alt={title}
         width={400}
         height={400}
-        className="w-[min(80vw,350px)] aspect-square rounded-3xl object-cover duration-300 group-hover:scale-110"
+        className="aspect-square w-[min(80vw,350px)] rounded-3xl object-cover duration-300 group-hover:scale-110"
         placeholder="blur"
       />
       <div className="absolute inset-0 bg-[linear-gradient(to_bottom,transparent,#000000cc)]"></div>
-      <div className="absolute right-0 bottom-4 left-3 text-white text-left">
+      <div className="absolute right-0 bottom-4 left-3 flex max-w-[40ch] flex-col gap-1 text-left text-white">
         <p className="font-montserrat text-[16px] font-bold capitalize">
           {title}
         </p>
