ef38fb8206b83d554814832336155efabbf1aa1a
Tom Okafor <tmeka008@gmail.com>
2025-06-28 21:01:41 +0100

    feat: add and style blog page text and content

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -3,21 +3,23 @@ import PageHeader from "@/components/page-header";
 import { UseGetCurrentBlog } from "../hooks/useGetCurrentBlog";
 import Image from "next/image";
 import icon from "../../../../public/blog-section/19HomesIcon.svg";
+import "../styles/styles.css";
 export default function Page() {
   const blogPost = UseGetCurrentBlog();
   return (
     <main>
       <PageHeader title={blogPost.title} />
-      <section className="mg:pd-16 w-100vw flex flex-col gap-10 p-13">
-        <div className="flex w-full flex-col gap-4 md:gap-8">
+      <section className="mg:pd-16 w-100vw flex flex-col gap-12 p-13">
+        <div className="flex w-full flex-col gap-4 md:gap-6">
           <Image
             src={blogPost.image}
             alt={blogPost.title}
-            width={800}
-            height={800}
+            width={2000}
+            height={2000}
             placeholder="blur"
-            className="bg-gold-25 w-full object-contain shadow-xl rounded-3xl"
+            className="w-full rounded-4xl object-contain"
           />
+          <p className="py-4 border-b border-t border-gold-75 font-montserrat font-semibold text-xs md:text-sm text-black-75 leading-[1.6] tracking-[2px] max-w-[40ch] md:max-w-[60ch]">{blogPost.description}</p>
           <div className="flex items-center gap-3">
             <Image src={icon} alt="19Homes logo" height={50} width={50} />
             <p className="font-poppins text-black-75 text-sm font-semibold md:text-lg">
@@ -25,9 +27,7 @@ export default function Page() {
             </p>
           </div>
         </div>
-        <p className="font-montserrat text-black-75 max-w-[45ch] text-sm leading-[130%] font-medium tracking-wider md:max-w-prose md:text-lg lg:text-xl">
-          {blogPost.description}
-        </p>
+        <div className="blog" dangerouslySetInnerHTML={{ __html: blogPost.content }} />
       </section>
     </main>
   );
