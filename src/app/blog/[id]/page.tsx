839599006114f9b6d3589d9e34ef03ddc25a8fa4
Tom Okafor <tmeka008@gmail.com>
2025-06-30 15:23:18 +0100

    style: add margin top to the blog image

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
   return (
     <main>
       <PageHeader title={blogPost.title} />
-      <section className="md:px-14 w-100vw flex flex-col gap-12 px-6">
+      <section className="md:px-14 w-100vw flex flex-col gap-12 px-6 mt-10 md:mt-16">
         <div className="flex w-full flex-col gap-4 md:gap-6">
           <Image
             src={blogPost.image}
