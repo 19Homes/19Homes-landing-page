60a1e73fc8f5bd162db85cbc9e6a1ea04431348e
Tom Okafor <tmeka008@gmail.com>
2025-07-02 23:58:41 +0100

    style: add entry animation to logo

--- src/components/header/components/logo/index.tsx
+++ src/components/header/components/logo/index.tsx
@@ -1,17 +1,31 @@
+"use client";
 import Image from "next/image";
 import logo from "../../../../../public/19Homes-icon.png";
+import { animate, createScope } from "animejs";
+import { useEffect, useRef } from "react";
 export default function Logo() {
+  const scope = useRef<ReturnType<typeof createScope> | null>(null);
+  const root = useRef(null);
+
+  useEffect(() => {
+    scope.current = createScope({ root }).add(() => {
+      animate(".logo", {
+        opacity: [{ to: 1, ease: "outQuad", duration: 200 }],
+        y: [{ from: "2rem", ease: "outQuad", duration: 200 }],
+      });
+    });
+  }, []);
   return (
-    <div className="mr-auto">
+    <div ref={root} className="mr-auto">
       <Image
         src="/19Homes-icon.svg"
         height={56}
         width={142}
         alt="19 Homes Logo"
-        className="hidden sm:block"
+        className="logo hidden opacity-0 sm:block"
       />
-      <div className="relative h-6 w-7.5">
-        <Image fill src={logo} alt="19Homes logo" className="sm:hidden" />
+      <div className="logo relative h-6 w-7.5 opacity-0 sm:hidden">
+        <Image fill src={logo} alt="19Homes logo" />
       </div>
     </div>
   );
