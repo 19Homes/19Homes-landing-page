a70b03343f7a78f8460155598d2ece26b5a3fba9
Tom Okafor <tmeka008@gmail.com>
2025-07-03 20:31:55 +0100

    style: add animation for featured-properties section

--- src/components/featured-properties/components/section-card/index.tsx
+++ src/components/featured-properties/components/section-card/index.tsx
@@ -9,8 +9,11 @@ export default function SectionCard({
   price,
 }: FeaturedPropertyCardType) {
   return (
-    <article className="shadow-card relative flex h-[392px] w-[232px] flex-col items-center rounded-[21px] md:rounded-4xl bg-white md:h-[591px] md:w-[350px]">
-      <div className="relative h-[232px] md:h-[350px] w-full overflow-hidden">
+    <article
+      className="shadow-card property-card relative flex h-[392px] w-[232px] flex-col items-center rounded-[21px] bg-white md:h-[591px] md:w-[350px] md:rounded-4xl"
+      style={{ opacity: 0, transform: "translateY(100px) scale(0.8)" }}
+    >
+      <div className="relative h-[232px] w-full overflow-hidden md:h-[350px]">
         <Image
           src={image}
           alt={name}
@@ -19,11 +22,11 @@ export default function SectionCard({
           placeholder="blur"
         />
       </div>
-      <div className="flex h-40 w-full flex-col items-center p-3 md:h-[241px] md:p-5">
+      <div className="flex h-40 w-full flex-col items-center p-3 md:h-[241px] md:p-5 bg-white rounded-[21px]">
         <SectionCardInfo name={name} price={price} />
         <CardButton />
       </div>
-      <div className="border-gold-25 absolute top-[-9px] left-[-9px] right-[-9px] -z-10 h-[394px] rounded-[21px] md:rounded-4xl border-2 md:top-[-14px] md:left-[-14px] md:h-[595px] md:w-[378px]"></div>
+      <div className="border-gold-25 absolute top-[-9px] right-[-9px] left-[-9px] -z-10 h-[394px] rounded-[21px] border-2 md:top-[-14px] md:left-[-14px] md:h-[595px] md:w-[378px] md:rounded-4xl"></div>
     </article>
   );
 }
--- src/components/featured-properties/index.tsx
+++ src/components/featured-properties/index.tsx
@@ -1,17 +1,53 @@
+"use client";
 import SectionCaption from "../sectionCaption";
 import { featuredPropertyCardDetails } from "./constants";
 import SectionCard from "./components/section-card";
+import { useEffect, useRef } from "react";
+import { animate, createScope, onScroll, stagger } from "animejs";
 
 export default function FeaturedProperties() {
+  const root = useRef(null);
+  const scope = useRef<ReturnType<typeof createScope> | null>(null);
+  useEffect(() => {
+    scope.current = createScope({ root }).add(() => {
+      animate(".background-section", {
+        opacity: [{ to: 1, ease: "outCirc", duration: 300 }],
+        scale: [{ to: 1, ease: "inOutBack", duration: 400 }],
+        autoplay: onScroll({
+          container: ".properties-container",
+          enter: "bottom-=400",
+          leave: "top",
+          sync: "play",
+        }),
+      });
+      animate(".property-card", {
+        opacity: 1,
+        y: "0px",
+        scale: 1,
+        delay: stagger(300),
+        ease: "inOutBack",
+        duration: 800,
+        autoplay: onScroll({
+          container: ".properties-container",
+          enter: "bottom-=400",
+          leave: "top",
+          sync: "play play reverse reverse",
+        }),
+      });
+    });
+  }, []);
   return (
-    <section className="flex flex-col items-center justify-center overflow-x-auto px-6 py-10 md:pl-14">
+    <section
+      ref={root}
+      className="properties-container flex flex-col items-center justify-center overflow-x-auto px-6 py-10 md:pl-14"
+    >
       <SectionCaption
         headText="our featured properties"
         subText="view our featured properties"
       />
       <section
-        className="flex w-full max-w-[1512px] gap-9 overflow-x-auto overflow-y-clip bg-[url('/featured-properties/graphic-vector.svg')] bg-center bg-no-repeat py-6 pl-3 md:gap-14 md:px-4 md:py-8 lg:justify-between"
-        style={{ scrollbarWidth: "none" }}
+        className="background-section flex w-full max-w-[1512px] gap-9 overflow-x-auto overflow-y-clip bg-[url('/featured-properties/graphic-vector.svg')] bg-contain bg-center bg-no-repeat py-6 pl-3 md:gap-14 md:px-4 md:py-8 lg:justify-between"
+        style={{ scrollbarWidth: "none", opacity: 0, transform: "scale(0.2)" }}
       >
         {featuredPropertyCardDetails.map((card, index) => (
           <SectionCard
