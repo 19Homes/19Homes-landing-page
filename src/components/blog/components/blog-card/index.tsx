95fe449b85f7c4febd7c9bd70450b9899a861838
Tom Okafor <tmeka008@gmail.com>
2025-07-09 11:32:36 +0100

    style: add svg filter effect to blog images

--- src/components/blog/components/blog-card/index.tsx
+++ src/components/blog/components/blog-card/index.tsx
@@ -11,6 +11,31 @@ export default function BlogCard({
 }) {
   return (
     <article className="flex w-[min(100%,342px)] flex-col gap-2.5 lg:w-[452px]">
+      <svg
+        width="0"
+        height="0"
+        viewBox="0 0 220 220"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <filter id="displacementFilter">
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.9"
+            numOctaves="2"
+            result="turbulence"
+          />
+          <feDisplacementMap
+            in2="turbulence"
+            in="SourceGraphic"
+            scale="200"
+            xChannelSelector="A"
+            yChannelSelector="A"
+          />
+        </filter>
+
+        <circle cx="100" cy="100" r="100" filter="url(#displacementFilter)" />
+      </svg>
+
       <Image
         src={source}
         alt={title}
@@ -18,6 +43,10 @@ export default function BlogCard({
         height={309}
         className="h-[234px] w-full object-cover lg:h-[309px] lg:w-[452px]"
         placeholder="blur"
+        style={{
+          filter: "url(#displacementFilter)",
+          transition: "all 300s ease",
+        }}
       />
       <div className="flex w-full flex-col gap-1.5 lg:gap-8">
         <h3 className="font-poppins text-black-100 text-lg font-bold capitalize lg:text-2xl">
