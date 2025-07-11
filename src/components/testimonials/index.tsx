99ffc4b03eef3da5ac5ebf87bd566276d1ad0226
Tom Okafor <tmeka008@gmail.com>
2025-07-11 01:21:12 +0100

    set up animation and create first animation for the cards

--- src/components/testimonials/index.tsx
+++ src/components/testimonials/index.tsx
@@ -1,9 +1,33 @@
+"use client";
 import { testimonialCards } from "./constants";
 import TestimonialCardBlock from "./components/testimonial-card";
+import { animate, createScope, createSpring, onScroll, stagger } from "animejs";
+import { useEffect, useRef } from "react";
 
 export default function Testimonials() {
+  const scope = useRef<ReturnType<typeof createScope> | null>(null);
+  const root = useRef(null);
+  useEffect(() => {
+    scope.current = createScope({ root }).add(() => {
+      animate(".testimonial-card", {
+        opacity: [{ to: 1, ease: "inOut", duration: 300 }],
+        y: [
+          {
+            from: "50px",
+            ease: "outCirc",
+            duration: 400,
+          },
+        ],
+        delay: stagger(300),
+      });
+    });
+    return () => scope.current?.revert();
+  }, []);
   return (
-    <section className="mx-auto flex w-full max-w-[1512px] justify-center px-4 lg:px-14 lg:pb-[44px]">
+    <section
+      ref={root}
+      className="mx-auto flex w-full max-w-[1512px] justify-center px-4 lg:px-14 lg:pb-[44px]"
+    >
       <div className="ml-8 flex w-full flex-col items-center gap-10 lg:gap-20">
         <h3 className="font-poppins text-black-100 text-[20px] font-bold lg:text-3xl">
           What <span className="text-gold-100">Our Clients</span> Say
