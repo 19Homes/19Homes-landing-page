ed36d86ea5dbc76609f7046b701baa7884910c72
Tom Okafor <tmeka008@gmail.com>
2025-07-13 20:29:33 +0100

    refactor: edit blog post titles

--- src/components/blog/constants/index.ts
+++ src/components/blog/constants/index.ts
@@ -5,19 +5,19 @@ import image3 from "../../../../public/blog-section/blog-post3.png";
 
 export const blogPosts: BlogPost[] = [
   {
-    title: "real estate investment benefits",
+    title: "Real Estate Investment Benefits",
     image: image1,
     description:
       "Discover how investing in real estate offers stability, passive income, long-term wealth, and protection against inflation",
   },
   {
-    title: "the future of real estate",
+    title: "The Future of Real Estate",
     image: image2,
     description:
       "Explore how technology, sustainability, and evolving lifestyles are shaping the exciting future of real estate investment.",
   },
   {
-    title: "the impact of interest rates on real estate",
+    title: "The Impact of Interest Rates on Real Estate",
     image: image3,
     description:
       "Understand how fluctuations in interest rates affect property values, mortgage costs, and the broader real estate market",
