83c3faf74c1951ed85a9bb0451e395fcfd269d78
Tom Okafor <tmeka008@gmail.com>
2025-07-15 11:34:34 +0100

    feat: add classname and styles as setup for animation

--- src/components/footer/components/footer-text-and-logo-block/index.tsx
+++ src/components/footer/components/footer-text-and-logo-block/index.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 export default function FooterTextAndLogoBlock() {
   return (
-    <div className="space-y-1 w-[85px] sm:w-[250px] lg:w-[332px] lg:space-y-4">
+    <div
+      className="flex w-[85px] flex-col gap-1 sm:w-[250px] lg:w-[332px] lg:gap-4 text-and-logo"
+      style={{ opacity: 0 }}
+    >
       <Image
         src="/19Homes-icon.svg"
         alt="19 home logo"
@@ -10,7 +13,7 @@ export default function FooterTextAndLogoBlock() {
         height={125}
         className="h-8 w-[86px] lg:h-[125px] lg:w-[331px]"
       />
-      <p className="font-montserrat text-[3.61px] font-medium sm:max-w-[45ch] text-white sm:text-[6px] lg:text-sm">
+      <p className="font-montserrat text-[3.61px] font-medium text-white sm:max-w-[45ch] sm:text-[6px] lg:text-sm">
         At 19Homes We Focus On Services Like House And Land Sales, Building And
         Architectural Designs. Work With Us To Bring Your Dream Home Into
         Reality.
