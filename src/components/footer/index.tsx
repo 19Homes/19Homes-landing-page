ccae388c0944132d360c6bd9092433602d5072df
Tom Okafor <tmeka008@gmail.com>
2025-07-15 12:30:24 +0100

    feat: sync animation with scroll

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,7 +2,7 @@
 import { useCallback } from "react";
 import FooterMainBlock from "./components/footer-main-block";
 import styles from "./styles/styles.module.css";
-import { animate, createScope, createSpring, stagger } from "animejs";
+import { animate, createScope, createSpring, onScroll, stagger } from "animejs";
 import { useEffect, useRef } from "react";
 export default function Footer() {
   const root = useRef(null);
@@ -13,6 +13,12 @@ export default function Footer() {
       animate(".text-and-logo", {
         opacity: [{ to: 1, ease: "outQuad", duration: 500 }],
         gap: [{ from: "100px", ease: "outBack", duration: 500 }],
+        autoplay: onScroll({
+          container: ".footer",
+          enter: "bottom-=100 top",
+          leave: "top+=500 bottom",
+          sync: "play play reverse reverse",
+        }),
       });
       animate(".footer-item, .footer-caption", {
         opacity: [{ to: 1, ease: "inOutCirc", duration: 200 }],
@@ -24,6 +30,12 @@ export default function Footer() {
           },
         ],
         delay: stagger(50),
+        autoplay: onScroll({
+          container: ".footer",
+          enter: "bottom top",
+          leave: "top+=500 bottom",
+          sync: "play play reverse reverse",
+        }),
       });
       animate(".footer-line", {
         opacity: [
@@ -40,10 +52,22 @@ export default function Footer() {
             duration: 5000,
           },
         ],
+        autoplay: onScroll({
+          container: ".footer",
+          enter: "bottom-=100 top",
+          leave: "top+=500 bottom",
+          sync: "play play reverse reverse",
+        }),
       });
       animate(".footer-sub-text", {
         opacity: [{ to: 1, ease: "inQuad", duration: 200 }],
         y: [{ to: 0, ease: "inOutBack(1.8)", duration: 500 }],
+        autoplay: onScroll({
+          container: ".footer",
+          enter: "bottom top",
+          leave: "top+=500 bottom",
+          sync: "play play reverse reverse",
+        }),
       });
     });
 
@@ -55,7 +79,7 @@ export default function Footer() {
   return (
     <footer
       ref={root}
-      className={`flex min-h-[129px] w-full flex-col gap-6 bg-[linear-gradient(rgba(20,20,20,0.92),rgba(20,20,20,0.75)),url('/footer/footer-image.png')] bg-cover px-6 py-[18px] lg:min-h-[502px] lg:gap-20 lg:px-14 lg:py-[49px] ${styles.footer}`}
+      className={`flex min-h-[129px] w-full flex-col gap-6 bg-[linear-gradient(rgba(20,20,20,0.92),rgba(20,20,20,0.75)),url('/footer/footer-image.png')] bg-cover px-6 py-[18px] lg:min-h-[502px] lg:gap-20 lg:px-14 lg:py-[49px] ${styles.footer} footer`}
     >
       <FooterMainBlock />
       <section className="flex w-full flex-col items-center gap-1.5 lg:gap-2.5">
